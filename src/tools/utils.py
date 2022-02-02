7a9fbc4c4fcaa28a940d74d9e7a41dc01b20b46b
Barree Guillaume <guillaume.barree@student-cs.fr>
2022-02-02 11:28:36 +0100

    [*+] Fix csv and add new linear model

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -1,5 +1,6 @@
 """This module aims to define utils function for the project."""
 from models.LinearNet_1 import LinearNet_1
+from models.LinearNet_2 import LinearNet_2
 
 
 def load_model(cfg, input_size, num_hidden_neuron):
@@ -13,5 +14,7 @@ def load_model(cfg, input_size, num_hidden_neuron):
     """
     if cfg["TRAIN"]["MODEL"] == "LinearNet_1":
         return LinearNet_1(num_features=input_size, num_hidden_neuron=num_hidden_neuron)
+    elif cfg["TRAIN"]["MODEL"] == "LinearNet_2":
+        return LinearNet_2(num_features=input_size)
     else:
         return LinearNet_1(num_features=input_size, num_hidden_neuron=num_hidden_neuron)
